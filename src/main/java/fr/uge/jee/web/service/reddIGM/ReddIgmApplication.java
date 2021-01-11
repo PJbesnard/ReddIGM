c8bda498fd6716e8113bb89dd0b246f41c5b8199
Pierrejean BESNARD <pj.besnard@outlook.com>
2021-01-11 20:53:48 +0100

    initial commit

---
+++ src/main/java/fr/uge/jee/web/service/reddIGM/ReddIgmApplication.java
@@ -0,0 +1,13 @@
+package fr.uge.jee.web.service.reddIGM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddIgmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddIgmApplication.class, args);
+    }
+
+}
