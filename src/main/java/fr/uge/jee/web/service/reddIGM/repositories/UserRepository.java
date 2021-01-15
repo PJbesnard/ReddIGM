63deb0a8769a9cd672b335b402e80305992df495
Samy WADAN <SaladeTomatOignon@hotmail.com>
2021-01-15 15:01:42 +0100

    feat: add security authentication with user/password
    In this version, all GET requests to /users/* must be authenticated with user and password.
    
    Note : stored  passwords are crypted using BCrypt.

--- src/main/java/fr/uge/jee/web/service/reddIGM/repositories/UserRepository.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/repositories/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-
+    @Query("select user from Users where username=:name")
+    User findByUsername(@Param("name") String username);
 }
--- src/main/java/fr/uge/jee/web/service/reddIGM/services/UserService.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/services/UserService.java
@@ -1,14 +1,35 @@
 package fr.uge.jee.web.service.reddIGM.services;
 
+import fr.uge.jee.web.service.reddIGM.models.User;
 import fr.uge.jee.web.service.reddIGM.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
 
+    public Optional<User> getById(Long id) {
+        return repository.findById(id);
+    }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username);
+
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        }
+
+        return user;
+    }
 }
