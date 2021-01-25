71210acd900fd3e7e2ac9fd14733c6f0af288865
LouisBillaut <louisbillaut2000@gmail.com>
2021-01-25 22:44:46 +0100

    fix: CommentRepository now use @Repository

--- src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
@@ -2,7 +2,9 @@ package fr.uge.jee.web.service.reddIGM.repositories;
 
 import fr.uge.jee.web.service.reddIGM.models.Comment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
     //List<Comment> findByPost(Post post);
 
