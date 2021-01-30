cac8344a5ad9e51d0ad02151a71ea8a9a49d8a0f
LouisBillaut <louisbillaut2000@gmail.com>
2021-01-30 23:39:00 +0100

    feat: comments are now linked to posts & users

--- src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
@@ -1,12 +1,16 @@
 package fr.uge.jee.web.service.reddIGM.repositories;
 
 import fr.uge.jee.web.service.reddIGM.models.Comment;
+import fr.uge.jee.web.service.reddIGM.models.Post;
+import fr.uge.jee.web.service.reddIGM.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
-    //List<Comment> findByPost(Post post);
+    List<Comment> findByPost(Post post);
 
-    // List<Comment> findAllByUser(User user);
+    List<Comment> findAllByUser(User user);
 }
