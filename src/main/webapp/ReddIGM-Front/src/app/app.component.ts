45533221c8eccbf392237428ce2a4f94a39d7dbf
Pierrejean BESNARD <pj.besnard@outlook.com>
2021-01-30 22:05:17 +0100

    feat: display post, some code refactored, nav bar & post component created

--- src/main/webapp/ReddIGM-Front/src/app/app.component.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // nom du composant html appelé pour l'injection
   selector: 'app-root',
+
+  // chemin jusqu'au code à injecter
   templateUrl: './app.component.html',
+
+  // chemin de son CSS 
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
