2b3fd8cb299ac67aa3e8eb1a79d1fa544fbc1774
GVSThierno <tbangour@guestviews.co>
2021-01-31 19:31:31 +0100

    update: create-post + ckeditor

--- src/main/webapp/ReddIGM-Front/src/app/app.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponent } from './pages/modals/create-post/create-post.component';
+import { CKEditorModule } from 'ckeditor4-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreatePostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	CKEditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/main/webapp/ReddIGM-Front/src/app/pages/modals/create-post/create-post.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.scss']
+})
+export class CreatePostComponent implements OnInit {
+	@Input() openModal: any
+	
+	@Output()
+  	openModelChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isModalActive: boolean = false;
+  isDropdownActive: boolean = false;
+  
+  openDropdown() {
+	this.isDropdownActive = !this.isDropdownActive;
+  }
+  closeModal() {
+	this.openModelChange.emit(!this.openModal);
+  }
+
+}
