e34b52649a1d57a329d35dd12fd7955645bcd244
Pierrejean BESNARD <pj.besnard@outlook.com>
2021-02-04 00:47:37 +0100

    feat: minor visual improvments added

--- src/main/webapp/ReddIGM-Front/src/app/app.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { PostViewComponent } from './post-view/post-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
