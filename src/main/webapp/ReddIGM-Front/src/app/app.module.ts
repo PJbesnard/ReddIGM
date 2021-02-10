1b1f6754846fd5f29ab716374c99b973193bdde0
LouisBillaut <louisbillaut2000@gmail.com>
2021-02-10 22:09:47 +0100

    feat: subs service created & implemented in browse-subs

--- src/main/webapp/ReddIGM-Front/src/app/app.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './pages/modals/create-post/create-post.component';
 import { CKEditorModule } from 'ckeditor4-angular';
 import { CardCreatePostComponent } from './card-create-post/card-create-post.component';
-import { BrowseSubsComponent } from './browse-subs/browse-subs.component';
+import { BrowseSubsComponent } from './subs/browse-subs/browse-subs.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PostViewComponent } from './post-view/post-view.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -23,9 +23,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-	AppRoutingModule,
-	CKEditorModule,
-  FontAwesomeModule
+	  AppRoutingModule,
+	  CKEditorModule,
+    FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
