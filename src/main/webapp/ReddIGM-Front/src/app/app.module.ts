e44fea52ddc06fe0c7ad32ca40e25ba5080b1ec8
Samy WADAN <SaladeTomatOignon@hotmail.com>
2021-02-11 18:36:56 +0100

    feat: add profile page

--- src/main/webapp/ReddIGM-Front/src/app/app.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { CommentPreviewComponent } from './comment-preview/comment-preview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
+    RegisterPageComponent,
+    ProfilePageComponent,
+    CommentPreviewComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/main/webapp/ReddIGM-Front/src/app/comment-preview/comment-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-preview',
+  templateUrl: './comment-preview.component.html',
+  styleUrls: ['./comment-preview.component.scss']
+})
+export class CommentPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
