28032234e53c2494bbaac0fddeda769b39fbd9d2
LouisBillaut <louisbillaut2000@gmail.com>
2021-02-20 22:56:43 +0100

    fix: set annotation to comment repository

--- src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/repositories/CommentRepository.java
@@ -4,10 +4,11 @@ import fr.uge.jee.web.service.reddIGM.models.Comment;
 import fr.uge.jee.web.service.reddIGM.models.Post;
 import fr.uge.jee.web.service.reddIGM.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
 
