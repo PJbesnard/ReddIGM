b9b4a3ee3a0201d7196d67cbf2b9e3db4d4fc456
LouisBillaut <louisbillaut2000@gmail.com>
2021-02-21 18:39:31 +0100

    feat: homePage component created

--- src/main/webapp/ReddIGM-Front/src/app/app.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListSubsComponent } from './subs/list-subs/list-subs.component';
 import { CardCreateSubComponent } from './subs/card-create-sub/card-create-sub.component';
 import { CreateSubComponent } from './subs/create-sub/create-sub.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { CreateSubComponent } from './subs/create-sub/create-sub.component';
     PostViewComponent,
     ListSubsComponent,
     CardCreateSubComponent,
-    CreateSubComponent
+    CreateSubComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
