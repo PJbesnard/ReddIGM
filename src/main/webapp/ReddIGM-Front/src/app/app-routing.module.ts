05aa34d173c56f1ac2c03067a7bcfd265ecffce1
Samy WADAN <SaladeTomatOignon@hotmail.com>
2021-02-28 00:42:26 +0100

    feat: update routes and add authguard when authentication is required

--- src/main/webapp/ReddIGM-Front/src/app/app-routing.module.ts
+++ src/main/webapp/ReddIGM-Front/src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
-import { PostViewComponent } from './post-view/post-view.component';
-import { CardCreatePostComponent } from './card-create-post/card-create-post.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListSubsComponent} from './subs/list-subs/list-subs.component';
 import {CreateSubComponent} from './subs/create-sub/create-sub.component'
 import {HomePageComponent} from './home-page/home-page.component'
 import {PostsFromSubComponent} from './posts-from-sub/posts-from-sub.component'
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { EditProfilePageComponent } from './edit-profile-page/edit-profile-page.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
-  {path:'', component: HomePageComponent},
-  {path: 'create-sub', component: CreateSubComponent},
-  {path:'posts-from-sub', component: PostsFromSubComponent},
-  {path: 'list-subs', component: ListSubsComponent},
-  {path: 'home', component: HomePageComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'users/:id', component: ProfilePageComponent},
+  {path: 'users/:id/edit', component: EditProfilePageComponent, canActivate: [AuthGuardService]},
+  {path: 'create-sub', component: CreateSubComponent, canActivate: [AuthGuardService]},
+  {path: 'posts-from-sub', component: PostsFromSubComponent},
+  {path: 'list-subs', component: ListSubsComponent}
 ];
 
 @NgModule({
