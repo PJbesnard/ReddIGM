0a2e89ecb6af64a90a1b97a35d58a660f104215a
LouisBillaut <louisbillaut2000@gmail.com>
2021-03-06 23:15:12 +0100

    feat: set picture and name of connected user

--- src/main/webapp/ReddIGM-Front/src/app/nav-bar/nav-bar.component.ts
+++ src/main/webapp/ReddIGM-Front/src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { AuthenticationService } from "../services/authentication.service"
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 
 @Component({
@@ -13,11 +15,22 @@ import { AuthenticationService } from "../services/authentication.service"
 export class NavBarComponent {
 	searchText: string = "";
   isAuthentified: boolean = false;
+  name: string | undefined;
+  user: User | null = new User();
+  picture: string = "";
 
-  constructor(private dataService: DataService, private authenticationService: AuthenticationService) {
+  constructor(private dataService: DataService, private authenticationService: AuthenticationService, private userService: UserService) {
     this.isAuthentified = this.authenticationService.isLoggedIn();
   }
 
+  ngOnInit(): void {
+    this.user = this.authenticationService.getCurrentUser();
+    let p = this.user?.getPicture();
+    if(p != null){
+      this.picture = p;
+      this.name = this.user?.username;
+    }
+  }
 
   search(){
 	  this.dataService.changeMessage(this.searchText)
