c0543bbc51ea7bb1ed1d2c73f66d5b4bc1ba44b4
Samy WADAN <SaladeTomatOignon@hotmail.com>
2021-03-07 04:35:48 +0100

    feat: adding user's votes recovery
    And removing useless Vote service

--- src/main/java/fr/uge/jee/web/service/reddIGM/repositories/VoteCommentRepository.java
+++ src/main/java/fr/uge/jee/web/service/reddIGM/repositories/VoteCommentRepository.java
@@ -3,6 +3,7 @@ package fr.uge.jee.web.service.reddIGM.repositories;
 import fr.uge.jee.web.service.reddIGM.models.Comment;
 import fr.uge.jee.web.service.reddIGM.models.User;
 import fr.uge.jee.web.service.reddIGM.models.VoteComment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface VoteCommentRepository extends CrudRepository<VoteComment, Long> {
+public interface VoteCommentRepository extends JpaRepository<VoteComment, Long> {
     Optional<VoteComment> findByCommentAndUser(Comment comment, User user);
     List<VoteComment> findAllByComment(Comment comment);
+    List<VoteComment> findAllByUserId(long userId);
+    List<VoteComment> findAllByUserIdOrderByCreationDateAsc(long userId);
+    List<VoteComment> findAllByUserIdOrderByCreationDateDesc(long userId);
 }
